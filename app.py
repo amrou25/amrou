f59cd56b4f5cc2fbfcc3b4654dd4409b2e06132d
amrou25 <asahinai754@gmail.com>
2021-04-14 14:26:14 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ['POST', 'GET'])
 def hello_world():
     return 'Hello, World!'
 if __name__ == "__main__":    
