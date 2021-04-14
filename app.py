f478fc406b954ddf89c28b1299f794c6db7cbeab
amrou25 <asahinai754@gmail.com>
2021-04-14 19:27:36 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask,redirect, url_for, request
-app = Flask(__name__)
+from flask import Flask,render_template,request
+from youtube_search import YoutubeSearch
+from pytube import YouTube
+app = Flask(__name__,template_folder='template')
 
-@app.route('/',methods = ['POST', 'GET'])
+@app.route('/',methods=["POST","GET"])
 def hello_world():
-    return 'Hello, World!'
+    return render_template('yt.html')
+
+@app.route('/res',methods=["POST","GET"])
+def res():
+    h=request.form['srch']
+    results = YoutubeSearch(h, max_results=200).to_json()
+    return results
+
+
+@app.route('/download',methods=["POST","GET"])
+def download():
+    url=request.form['url']
+    YouTube(url).streams.first().download()
+    return url
+
+
 if __name__ == "__main__":    
    app.run(debug=True)
